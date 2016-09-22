cd76f9f1d4d6a82cb8c10ec977154ab00b727991
Hassan Niazi <sikander_niazi1@hotmail.com>
2016-09-23 01:28:04 +0500

    pHONE # sIGN iN

--- app/src/main/java/com/example/hash/bloodbank/LoginActivity.java
+++ app/src/main/java/com/example/hash/bloodbank/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.example.hash.bloodbank;
 
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,18 +8,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.digits.sdk.android.AuthCallback;
+import com.digits.sdk.android.Digits;
+import com.digits.sdk.android.DigitsAuthButton;
+import com.digits.sdk.android.DigitsException;
+import com.digits.sdk.android.DigitsSession;
+
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import io.fabric.sdk.android.Fabric;
 
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
-    //Firebase Declarations
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "Fto25uhjdYYi7hvrLHiZTq7xa";
+    private static final String TWITTER_SECRET = "Fa7xPIip9FxSNzowmyRcBCtXh7EXh8GCGSWYHmUvBZdCDpENek";
+
 
     // UI Controls
 
@@ -30,33 +35,33 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new TwitterCore(authConfig), new Digits.Builder().build());
+
+
+
         setContentView(R.layout.activity_login);
         //UI Initialization
-        emailSignIn = (Button) findViewById(R.id.signInWithEmailButton);
-        emailSignUp = (Button) findViewById(R.id.signUpWithEmailButton);
-        emailEditText = (EditText) findViewById(R.id.emailAddressText);
-        passwordEditText = (EditText) findViewById(R.id.passwordText);
+
 
         //UI Listeners
-        emailSignIn.setOnClickListener(this);
-        emailSignUp.setOnClickListener(this);
 
-        // Firebase Auth Listener
-        mAuth = FirebaseAuth.getInstance();
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+        //Digits
+        DigitsAuthButton digitsButton = (DigitsAuthButton) findViewById(R.id.auth_button);
+        digitsButton.setCallback(new AuthCallback() {
             @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // User is signed in
-//                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                } else {
-                    // User is signed out
-//                    Log.d(TAG, "onAuthStateChanged:signed_out");
-                }
-
+            public void success(DigitsSession session, String phoneNumber) {
+                // TODO: associate the session userID with your user model
+                Toast.makeText(getApplicationContext(), "Authentication successful for "
+                        + phoneNumber, Toast.LENGTH_LONG).show();
             }
-        };
+
+            @Override
+            public void failure(DigitsException exception) {
+                Log.d("Digits", "Sign in with Digits failure", exception);
+            }
+        });
 
 
     }
@@ -64,100 +69,19 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
+
     }
 
     @Override
     public void onStop() {
-        super.onStop();
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
-        }
+
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId())
-        {
-            case R.id.signInWithEmailButton:
-
-                if(loginFieldsNotEmpty())
-                {
-                    signInUserWithEmail(emailEditText.getText().toString(),passwordEditText.getText().toString());
-                }
-                break;
-
-            case R.id.signUpWithEmailButton:
-
-                if(loginFieldsNotEmpty())
-                {
-                    registerNewUserToDatabase(emailEditText.getText().toString(),passwordEditText.getText().toString());
-                }
-                break;
-        }
 
     }
 
-    private boolean loginFieldsNotEmpty(){
 
-        if(emailEditText.getText().toString().trim().equals("") || passwordEditText.getText().toString().trim().equals("")){
-            if(emailEditText.getText().toString().trim().equals("") )
-            {
-                emailEditText.setError("Please enter Email Address");
-            }
-            if(passwordEditText.getText().toString().trim().equals("") )
-            {
-                passwordEditText.setError("Please enter Password");
-            }
-
-            return false;
-        }
-        else {
-            return  true;
-        }
-
-    }
-
-    private void registerNewUserToDatabase(String email, String password)
-    {
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d("1000", "createUserWithEmail:onComplete:" + task.isSuccessful());
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        if (!task.isSuccessful()) {
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-
-
-                    }
-                });
-    }
-
-    private void signInUserWithEmail(String email, String password){
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d("1001", "signInWithEmail:onComplete:" + task.isSuccessful());
-
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        if (!task.isSuccessful()) {
-                            Log.w("1001", "signInWithEmail", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-
-                        // ...
-                    }
-                });
-    }
 
 }
