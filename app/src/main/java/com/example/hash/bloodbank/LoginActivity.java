7e4b89adbc1c0b1fd3b7dc188b292f0ba4b78d44
Hassan Niazi <sikander_niazi1@hotmail.com>
2016-09-22 23:58:30 +0500

    Email SignIn & SignUp Added

--- app/src/main/java/com/example/hash/bloodbank/LoginActivity.java
+++ app/src/main/java/com/example/hash/bloodbank/LoginActivity.java
@@ -4,22 +4,44 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
     //Firebase Declarations
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
+    // UI Controls
+
+    Button emailSignIn , emailSignUp;
+    EditText emailEditText,passwordEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    // Firebase Auth Listener
+        //UI Initialization
+        emailSignIn = (Button) findViewById(R.id.signInWithEmailButton);
+        emailSignUp = (Button) findViewById(R.id.signUpWithEmailButton);
+        emailEditText = (EditText) findViewById(R.id.emailAddressText);
+        passwordEditText = (EditText) findViewById(R.id.passwordText);
+
+        //UI Listeners
+        emailSignIn.setOnClickListener(this);
+        emailSignUp.setOnClickListener(this);
+
+        // Firebase Auth Listener
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -32,7 +54,7 @@ public class LoginActivity extends AppCompatActivity {
                     // User is signed out
 //                    Log.d(TAG, "onAuthStateChanged:signed_out");
                 }
-                // ...
+
             }
         };
 
@@ -52,6 +74,90 @@ public class LoginActivity extends AppCompatActivity {
         if (mAuthListener != null) {
             mAuth.removeAuthStateListener(mAuthListener);
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.signInWithEmailButton:
+
+                if(loginFieldsNotEmpty())
+                {
+                    signInUserWithEmail(emailEditText.getText().toString(),passwordEditText.getText().toString());
+                }
+                break;
+
+            case R.id.signUpWithEmailButton:
+
+                if(loginFieldsNotEmpty())
+                {
+                    registerNewUserToDatabase(emailEditText.getText().toString(),passwordEditText.getText().toString());
+                }
+                break;
+        }
+
+    }
+
+    private boolean loginFieldsNotEmpty(){
+
+        if(emailEditText.getText().toString().trim().equals("") || passwordEditText.getText().toString().trim().equals("")){
+            if(emailEditText.getText().toString().trim().equals("") )
+            {
+                emailEditText.setError("Please enter Email Address");
+            }
+            if(passwordEditText.getText().toString().trim().equals("") )
+            {
+                passwordEditText.setError("Please enter Password");
+            }
+
+            return false;
+        }
+        else {
+            return  true;
+        }
+
+    }
+
+    private void registerNewUserToDatabase(String email, String password)
+    {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("1000", "createUserWithEmail:onComplete:" + task.isSuccessful());
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    }
+                });
+    }
+
+    private void signInUserWithEmail(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("1001", "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w("1001", "signInWithEmail", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
 
 }
